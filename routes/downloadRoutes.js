5de98bf5559b37807154d5daca442da6ac8e2cd5
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-09-03 19:34:39 -0400

    Initial Commit

---
+++ routes/downloadRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/download/:filename', (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, '../files/applications/', filename);
+
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error('Error downloading file:', err);
+      res.status(500).json({ success: false, message: 'File not found or server error' });
+    }
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const loginRoutes = require('./routes/login');
+const downloadRoutes = require('./routes/downloadRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(`Received POST request to ${req.url}`);
+    console.log('Request data:', req.body);
+  }
+  next();
+});
+
+app.use('/api', loginRoutes);
+app.use('/api', downloadRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
