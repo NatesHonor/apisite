f12f0df0d09c9f32e0a88f447cf061bb8c28adbe
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-09-05 18:45:25 -0400

    Updated downloadroute

--- routes/downloadRoutes.js
+++ routes/downloadRoutes.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
 router.get('/:filename', (req, res) => {
   const { filename } = req.params;
-  const filePath = path.join(__dirname, '../files/applications/', filename);
-
-  res.download(filePath, (err) => {
-    if (err) {
-      console.error('Error downloading file:', err);
-      res.status(500).json({ success: false, message: 'File not found or server error' });
-    }
-  });
+  const filePathWithExtension = path.join(__dirname, '../files/applications/', filename);
+  const filePathWithoutExtension = path.join(__dirname, '../files/applications/', `${filename}.exe`); // Assuming .exe as the extension
+  if (fs.existsSync(filePathWithExtension)) {
+    res.download(filePathWithExtension, (err) => {
+      if (err) {
+        console.error('Error downloading file:', err);
+        res.status(500).json({ success: false, message: 'File not found or server error' });
+      }
+    });
+  } else if (fs.existsSync(filePathWithoutExtension)) {
+    res.download(filePathWithoutExtension, (err) => {
+      if (err) {
+        console.error('Error downloading file:', err);
+        res.status(500).json({ success: false, message: 'File not found or server error' });
+      }
+    });
+  } else {
+    res.status(404).json({ success: false, message: 'File not found' });
+  }
 });
 
 module.exports = router;
