e8a52a09e46127102538d361fa191b897623f70b
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-06 15:54:31 -0400

    Updated ticket schema

--- models/Ticket.js
+++ models/Ticket.js
@@ -18,6 +18,6 @@ const ticketSchema = new mongoose.Schema({
   closedAt: { type: Date }
 });
 
-const Ticket = mongoose.model('Ticket', ticketSchema);
+const Ticket = mongoose.model('Ticket', ticketSchema, 'NatesServicesSupport');
 
 module.exports = Ticket;
