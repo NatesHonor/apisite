4b9ef5eef4fbedd7cf62ccb9cbc5b98f4f045a88
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-10 03:32:51 -0400

    Updated career schema

--- models/Career.js
+++ models/Career.js
@@ -3,15 +3,19 @@ const mongoose = require('mongoose');
 const careerSchema = new mongoose.Schema({
   jobID: { type: Number, required: true, unique: true },
   remote: { type: Boolean, default: true },
+  positions: { type: Number, default: 1 },
   jobTitle: { type: String, required: true },
   postingDate: { type: Date, default: Date.now },
   shortDescription: { type: String, required: true },
   requirements: { type: String, required: true },
   customQuestions: [{ type: String }],
+  salary: { type: String, required: true },
   company: { type: String, required: true },
   tags: [{ type: String }],
-  category: { type: String, required: true } 
+  category: { type: String, required: true },
+  type: { type: String }
 });
+
 
 const Career = mongoose.model('Career', careerSchema);
 
